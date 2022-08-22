ec6116d61dbf4c6eaae2e7adb3c5981ca4a5664c
JoaoDant4s <joaob1226@hotmail.com>
2022-08-22 09:45:25 -0300

    fundamentos do react

---
+++ 2-fundamentos/fundamentos/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+//components
+import FirstComponent from "./components/FirstComponent"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
