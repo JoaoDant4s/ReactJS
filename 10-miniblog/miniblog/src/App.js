530da7bac9681002aa05524b1bb5cc9c425c90d4
JoaoDant4s <joaob1226@hotmail.com>
2022-09-15 08:39:38 -0300

    trocado estrutura de dialog para modal

--- 10-miniblog/miniblog/src/App.js
+++ 10-miniblog/miniblog/src/App.js
@@ -51,7 +51,7 @@ function App() {
               />
               <Route 
                 path="/register" 
-                element={!user ? <Register /> : <Navigate to="/" />} 
+                element={!user ? <Register /> : <Navigate to="/" />}
               />
               {/*<Route path="/new-post" element={<NewPost />} />
               <Route path="/dashboard" element={<Dashboard />} />
